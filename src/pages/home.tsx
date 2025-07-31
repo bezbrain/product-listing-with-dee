25c01f141fad7a0f702bd206b31a938de9fb606e
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-07-31 04:44:17 +0100

    start of work

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is Home Page</h1>
-      <Link to="/products">Go to Product Page</Link>
+      <div></div>
+      {/* <Link to="/products">Go to Product Page</Link> */}
     </div>
   );
 };
