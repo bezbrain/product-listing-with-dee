05b231cbf1b0b845167d64e80d5724125ff576d2
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-08-01 05:25:05 +0100

    color effect added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,16 +11,18 @@ const Home = () => {
           Affordability meet.
         </h2>
         <p>
-          At Precisious Auto Sales, we are committed to providing the public
-          with dependable, high-quality luxury vehicles at competitive prices
-          all with a focus on convenience and customer satisfaction. We offer
-          flexible financing options as well as the ability to pay in full,
-          making the buying process seamless for every customer.
+          At <span>Precisious Auto Sales</span> , we are committed to providing
+          the public with <span>dependable, high-quality luxury vehicles</span>{" "}
+          at competitive prices all with a focus on convenience and customer
+          satisfaction. We offer flexible financing options as well as the
+          ability to pay in full, making the buying process seamless for every
+          customer.
         </p>
         <p>
-          We invite you to explore our About Us and Services sections to learn
-          more about how we operate and how you can take advantage of our
-          exceptional offers.
+          We invite you to explore our <span>About Us</span> and{" "}
+          <span>Our Services </span>
+          sections to learn more about how we operate and how you can take
+          advantage of our exceptional offers.
         </p>
       </div>
       {/* <Link to="/products">Go to Product Page</Link> */}
