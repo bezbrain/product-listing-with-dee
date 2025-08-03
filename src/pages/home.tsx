7bdd989bd1b38ae5217f6419c5f5bc988649161f
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-08-03 02:41:27 +0100

    service section done

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -71,23 +71,40 @@ const Home = () => {
         <p>
           At Precisious, we make purchasing a vehicle simple, transparent, and
           customer-focused. Our process begins once a reservation fee or down
-          payment is made. For full payments, a 70% deposit is collected
-          upfront, with the remaining balance due upon finalization. For
-          financing, the process begins after the down payment is completed, and
-          ongoing payment terms will be arranged by our customer service team.
-          Once the down payment is confirmed, we handle all the necessary
+          payment is made. For full payments, a{" "}
+          <span>70% deposit is collected upfront </span> , with the remaining
+          balance due upon finalization.
+        </p>
+        <p>
+          For financing,{" "}
+          <span>the process begins after the down payment is completed </span> ,
+          and ongoing payment terms will be arranged by our customer service
+          team. Once the down payment is confirmed, we handle all the necessary
           paperwork and arrange for our courier service to deliver the vehicle
           directly to your address. If a monthly payment is missed under a
-          financing plan, a $25 late fee (or an amount agreed upon in the
-          contract) will be applied. Each vehicle comes with: No additional
-          mileage Two keys Bill of sale Temporary tag Clean title You will have
-          two days to test drive the vehicle or have it inspected by your
-          mechanic. If you choose to keep it, you’ll sign the final documents
-          and either complete the payment or proceed with your financing
-          agreement. If you decide not to keep the vehicle within the two-day
-          test period, simply notify us, and our courier will arrange pickup.
-          Your deposit will be refunded. Our goal is to ensure a smooth,
-          convenient, and risk-free car buying experience.
+          financing plan, a <span>$25 late fee </span> (or an amount agreed upon
+          in the contract) will be applied.
+        </p>
+        <h3>Each vehicle comes with:</h3>
+        <ul>
+          <li>No additional mileage</li>
+          <li>Two keys</li>
+          <li>Bill of sale</li>
+          <li>Temporary tag</li>
+          <li>Clean title</li>
+        </ul>
+
+        <p>
+          You will have <span>two days </span> to test drive the vehicle or have
+          it inspected by your mechanic. If you choose to keep it, you’ll sign
+          the final documents and either complete the payment or proceed with
+          your financing agreement. If you decide not to keep the vehicle within
+          the two-day test period, simply notify us, and our courier will
+          arrange pickup. Your deposit will be refunded.{" "}
+        </p>
+        <p className="goal">
+          Our goal is to ensure a smooth, convenient, and risk-free car buying
+          experience.
         </p>
       </div>
       {/* end of our service */}
