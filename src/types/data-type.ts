51731a27fdb09b94f01c6c622b84e4dc7ca208bb
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-08-04 02:59:15 +0100

    data type added

--- src/types/data-type.ts
+++ src/types/data-type.ts
@@ -1,1 +1,20 @@
-
+export interface DataProp {
+  id: string | number;
+  image: string;
+  details: string;
+  miles: string;
+  price: string;
+  deposite: string;
+  instock: string;
+  warranty: string;
+  detailes1: string;
+  detailes2: string;
+  detailes3: string;
+  detailes4: string;
+  detailes5: string;
+  detailes6: string;
+  detailes7: string;
+  detailes8: string;
+  detailes9: string;
+  detailes10: string;
+}
